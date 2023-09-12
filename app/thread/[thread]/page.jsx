d528dbfa1cb53e1e65e290dd273b6adda979fcbb
godley0623 <godley11691@yahoo.com>
2023-09-12 13:11:35 -0400

    added buttons to Thread page

--- app/thread/[thread]/page.jsx
+++ app/thread/[thread]/page.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'next/navigation' 
 import axios from 'axios'
 import ReplyDisplay from '@/app/components/ReplyDisplay'
+import MainButton from '@/app/components/MainButton'
 
 export default function page() {
     const serverUrl = 'http://localhost:4000/'
@@ -11,6 +12,7 @@ export default function page() {
     const threadNum = Number(params.thread)
 
     const [thread, setThread] = useState({})
+    const [postToggle, setPostToggle] = useState(false)
 
     useEffect(() => {
       axios.get(`${serverUrl}threads/${threadNum}`)
@@ -21,8 +23,30 @@ export default function page() {
       })
     }, [])
 
+    function toggleForm() {
+      setPostToggle(!postToggle)
+    }
+
   return (
     <div>
+      <div className='w-full flex flex-col items-center border-borderColor border-b-2'> 
+        <h1 className='text-light text-3xl font-bold font-tahoma'>Cartoons & Comics</h1>
+        <p className="text-light">Discuss your favorite cartoon shows</p>
+      </div>
+
+      <div className='flex justify-center'>
+        <MainButton text='Return'/>
+        <MainButton text='Bottom'/>
+        <MainButton text='Update'/>
+      </div>
+
+      {!postToggle &&<div className='flex justify-center mb-4 border-borderColor border-b-2'>
+        <MainButton click={toggleForm} text='Post Reply'/>
+      </div>}
+      {postToggle &&<div className='flex justify-center mb-4 border-borderColor border-b-2'>
+        <MainButton click={toggleForm} text='Close Post Form'/>
+      </div>}
+
       {JSON.stringify(thread) !== '{}' && 
         <ReplyDisplay op={true} reply={thread}/>
       }
